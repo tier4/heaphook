9155c48a6a054ef4f2d09e4462317f4c0bee8d51
wep21 <daisuke.nishimatsu1021@gmail.com>
2023-08-12 01:30:30 +0900

    fix: add workaround to build on ubuntu 20.04
    Signed-off-by: wep21 <daisuke.nishimatsu1021@gmail.com>

--- src/preloaded_tlsf.cpp
+++ src/preloaded_tlsf.cpp
@@ -370,6 +370,7 @@ struct mallinfo mallinfo()
   return orig();
 }
 
+#ifdef HAVE_MALLINFO2
 using mallinfo2_type = struct mallinfo2 (*)( void);
 struct mallinfo2 mallinfo2()
 {
@@ -377,6 +378,7 @@ struct mallinfo2 mallinfo2()
   printf("hoge: mallinfo2 called\n");
   return orig();
 }
+#endif
 
 using mallopt_type = int (*)(int, int);
 int mallopt(int param, int value)
